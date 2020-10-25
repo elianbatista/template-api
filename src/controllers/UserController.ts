1f795a9853c229fbc7c6259d56f7193b706630be
Elian Batista <elianbatista2000@gmail.com>
2020-10-25 17:24:27 -0300

    Configuração do Overnight

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express'
+import { Controller, Get, Post } from '@overnightjs/core'
 
 import User from '../schemas/User'
 
+@Controller('users')
 class UserController {
+  @Get('')
   public async index (req: Request, res: Response): Promise<Response> {
     const users = await User.find()
 
     return res.json(users)
   }
 
+  @Post('')
   public async store (req: Request, res: Response): Promise<Response> {
     const user = await User.create(req.body)
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,39 @@
-import app from './app'
+import { Server } from '@overnightjs/core'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
-app.listen(3333)
+import userController from './controllers/UserController'
+
+export class SetupServer extends Server {
+  public constructor (private port = 3333) {
+    super()
+  }
+
+  public init (): void {
+    this.setupExpress()
+    this.setupControllers()
+    this.database()
+    this.app.listen(this.port, () => {
+      console.log('Server listening on port: ' + this.port)
+    })
+  }
+
+  private setupExpress (): void {
+    this.app.use(bodyParser.json())
+    this.app.use(cors())
+  }
+
+  private setupControllers (): void {
+    this.addControllers([
+      userController
+    ])
+  }
+
+  private database (): void {
+    mongoose.connect('mongodb://localhost:27017/tsnode', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  }
+}
